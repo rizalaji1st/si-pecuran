a3f1d665b6bc8c43bcb8ab36444a827565dd4c5f
rizalaji1st <rizalaji561@gmail.com>
2022-07-21 08:14:27 +0700

    Menambah logo asset

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -39,7 +39,7 @@
 					<div class="aside-logo flex-column-auto" id="kt_aside_logo">
 						<!--begin::Logo-->
 						<a href="../../demo1/dist/index.html">
-							<img alt="Logo" src="{{ asset('metronic/assets/media/logos/logo-1-dark.svg')}}" class="h-25px logo" />
+							<img alt="Logo" src="{{ asset('metronic/assets/media/logos/sipecuran-light.png')}}" class="h-30px logo" />
 						</a>
 						<!--end::Logo-->
 						<!--begin::Aside toggler-->
@@ -197,7 +197,7 @@
 							<!--begin::Mobile logo-->
 							<div class="d-flex align-items-center flex-grow-1 flex-lg-grow-0">
 								<a href="../../demo1/dist/index.html" class="d-lg-none">
-									<img alt="Logo" src="{{ asset('metronic/assets/media/logos/logo-2.svg')}}" class="h-30px" />
+									<img alt="Logo" src="{{ asset('metronic/assets/media/logos/sipecuran-logo.png')}}" class="h-40px" />
 								</a>
 							</div>
 							<!--end::Mobile logo-->
